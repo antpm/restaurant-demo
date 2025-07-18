3e15a48bb81b325d26fedcfe808e2d81a2f32804
Anthony Moores <anthonypm.29590@gmail.com>
2025-07-18 08:44:09 -0300

    Made footer bg darker

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@ import { FaCalendar, FaMapLocation, FaPhone } from 'react-icons/fa6';
 
 export default function Footer() {
     return (
-        <div className='fixed flex md:flex-row flex-col bottom-0 left-0 w-full bg-black/50 justify-around items-center p-4 z-50'>
+        <div className='fixed flex md:flex-row flex-col bottom-0 left-0 w-full bg-black/75 justify-around items-center p-4 z-50'>
             <div className='flex flex-row items-center gap-2'>
                 <FaPhone className='md:w-6 md:h-6 h-4 w-4 text-white' />
                 <p className='md:text-xl text-white'>555-555-5555</p>
